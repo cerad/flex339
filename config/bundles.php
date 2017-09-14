4644027f6e445f9f97382ba709d054d0e3cc99a9
ahundiak <ahundiak@gmail.com>
2017-09-14 08:33:30 -0500

    Added doctrine-bundle

--- config/bundles.php
+++ config/bundles.php
@@ -3,4 +3,6 @@
 return [
     'Symfony\Bundle\FrameworkBundle\FrameworkBundle' => ['all' => true],
     'Symfony\Bundle\WebServerBundle\WebServerBundle' => ['dev' => true],
+    'Doctrine\Bundle\DoctrineCacheBundle\DoctrineCacheBundle' => ['all' => true],
+    'Doctrine\Bundle\DoctrineBundle\DoctrineBundle' => ['all' => true],
 ];
