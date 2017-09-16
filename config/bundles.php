c7439f8cf9acec5f2cd0b53383ed5d2c3b5a0035
ahundiak <ahundiak@gmail.com>
2017-09-16 13:06:23 -0500

    Added debug and security packs

--- config/bundles.php
+++ config/bundles.php
@@ -6,4 +6,7 @@ return [
     'Doctrine\Bundle\DoctrineCacheBundle\DoctrineCacheBundle' => ['all' => true],
     'Doctrine\Bundle\DoctrineBundle\DoctrineBundle' => ['all' => true],
     'Symfony\Bundle\MonologBundle\MonologBundle' => ['all' => true],
+    'Symfony\Bundle\WebProfilerBundle\WebProfilerBundle' => ['dev' => true, 'test' => true],
+    'Symfony\Bundle\TwigBundle\TwigBundle' => ['all' => true],
+    'Symfony\Bundle\SecurityBundle\SecurityBundle' => ['all' => true],
 ];
